e8a8e7fef95c309a86c17b8cdcdabf7dabc07382
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-03-12 03:48:26 +0000

    [Add] 投稿機能の作成(4)

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,12 +6,13 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_imege.save
+    @post_image.save
     redirect_to post_images_path
   end
 
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
