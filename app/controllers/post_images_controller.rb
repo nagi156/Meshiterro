4918688b678a0405a4b17b6a46ed0f7fd5238a10
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-03-13 11:48:06 +0000

    [Add] ページネーション追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,17 +6,17 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    
+
     if  @post_image.save
       redirect_to post_images_path
-    else  
+    else
       render :new
     end
   end
 
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
