0095f82a54449d3cef4780b327292b6f7786d4aa
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-03-14 10:34:51 +0000

    [Add] アクセス権限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,33 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+
     @user = User.find(params[:id])
   end
-  
+
   def update
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
   
+  def is_maching_
+    
+  end
+
 end
